d14080ebd0d49dd166baed2314530720b671bb74
望月祐汰 <mczk9402@gmail.com>
2021-12-09 02:01:00 +0900

    type literal

---
+++ src/literal.ts
@@ -0,0 +1,12 @@
+export {};
+
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土' = '日';
+dayOfTheWeek = '月';
+// dayOfTheWeek = '123';
+// 有限のパターンにしたい時にリテラル型を使う
+
+let manth: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+manth = 12;
+// manth = 13;
+
+let TRUE: true = true;
