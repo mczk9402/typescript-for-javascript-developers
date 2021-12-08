3b8b6368b7b4b96dcc9f42ecbffd9f21b32d23ea
望月祐汰 <mczk9402@gmail.com>
2021-12-09 01:20:47 +0900

    type unknown & type gard

--- src/unknown.ts
+++ src/unknown.ts
@@ -1,1 +1,17 @@
-export {}
+export {};
+
+const kansu = (): number => 43;
+
+let numberAny: any = kansu();
+let numberUnknown: unknown = kansu();
+
+let sumAny = numberAny + 10;
+
+// エラー 型が不明だから計算ができない事を示唆
+// let sumUnknown = numberUnknown + 10;
+
+// type gard タイプガード 特定の型かを確認するのをタイプガードという
+if (typeof numberUnknown === 'number') {
+  // number型なら処理が走る
+  let sumUnknown = numberUnknown + 10;
+}
