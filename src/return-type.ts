c30cd7ea597d8af8e5e62b920489186f342558e9
望月祐汰 <mczk9402@gmail.com>
2021-12-11 23:13:40 +0900

    return-type

---
+++ src/return-type.ts
@@ -0,0 +1,14 @@
+export {};
+
+// 関数の値を返す型
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log(add(1, 2));
+
+type ReturnTypeFromAdd = ReturnType<typeof add>;
+
+/*
+  type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+*/
