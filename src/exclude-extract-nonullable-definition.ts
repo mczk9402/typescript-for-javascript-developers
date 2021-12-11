96315c898e01ea350920b8a62524583aad434552
望月祐汰 <mczk9402@gmail.com>
2021-12-11 21:56:26 +0900

    exclude-extract-nonullable-definition

---
+++ src/exclude-extract-nonullable-definition.ts
@@ -0,0 +1,29 @@
+export {};
+
+type MyExclude = DebugType;
+
+type DebugType = () => void;
+type SomeTypes = string | number | DebugType;
+// ユニオン型から除去する型 Exclude<ユニオンタイプ, 除去する型>
+type FunctionType = Exclude<SomeTypes, string | number>;
+type MyFunctionType = MyExclude;
+
+type NunFunctionType = Exclude<SomeTypes, DebugType>;
+type TypeExcludingFunction = Exclude<SomeTypes, Function>;
+// Function 関数の型の総称
+// extract ユニオン型から指定の型を継承する
+type FunctionTypeExtract = Extract<SomeTypes, DebugType>;
+type NonFunctionTypeByExtract = Extract<SomeTypes, string | number>;
+// すべての関数は引き継ぐ
+type FunctionTypeExtractingFunction = Extract<SomeTypes, Function>;
+
+// nullを抜く
+type NullableTypes = string | number | null | undefined;
+type NonNullableTypes = NonNullable<NullableTypes>; //nullを取る
+
+// type NonNullable<T> = T extends null | undefined ? never : T;
+type a =
+  | (string extends null | undefined ? never : string) // true never
+  | (number extends null | undefined ? never : number) // true never
+  | (null extends null | undefined ? never : null) // false
+  | (undefined extends null | undefined ? never : undefined); // false
